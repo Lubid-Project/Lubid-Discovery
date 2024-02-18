da6387d6bd878a8c918d16ee66a1bd8c6bd8da2b
dpound@naver.com <dpound@naver.com>
2024-02-18 18:40:07 +0900

    lubid 유레카 디스커버리 프로젝트 생성 및 뼈대 구현중

---
+++ src/test/java/com/lubid/lubiddiscovery/LubidDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubid.lubiddiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LubidDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
